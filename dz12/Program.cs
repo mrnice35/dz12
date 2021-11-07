bd8e43a8ddab5a058245af7bef62a0d771790432
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-11-07 21:18:48 +0300

    Добавьте файлы проекта.

---
+++ dz12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("введите значение точки по оси координат X");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите значение точки по оси координат Y");
+            int y = Convert.ToInt32(Console.ReadLine());*/
+            Circle circle = new Circle();
+            circle.ToCircumference();
+            circle.ToArea();
+            circle.InACircle();
+            Console.ReadKey();
+
+        }
+
+    }
+    class Circle
+    {
+        public static double r = 15;
+        public static double x0 = 3;
+        public static double y0 = 4;
+
+        public void ToCircumference()
+        {
+            double L = 2 * Math.PI * r;
+            Console.WriteLine("Длина окржуности равна {0}", L);
+        }
+        public void ToArea()
+        {
+            double S = Math.PI * r * r;
+            Console.WriteLine("Площадь круга равна {0}", S);
+        }
+        public void InACircle(double x1=50, double y1 = 4)
+        {
+            if (Math.Pow(x0 - x1, 2) + Math.Pow(y0 - y1, 2) < r * r)
+            {
+             Console.WriteLine("точка лежит внутри круга");
+            }
+            else
+            {
+             Console.WriteLine("точка лежит вне заданного круга");
+            }
+        }
+    }
+}
+
+            
+
+           

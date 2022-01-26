00f851ba8e9f657089d191a621073e59874e8ad2
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2022-01-26 20:05:42 +0300

    1

--- dz12/Program.cs
+++ dz12/Program.cs
@@ -6,53 +6,55 @@ using System.Threading.Tasks;
 
 namespace dz12
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             /*Console.WriteLine("введите значение точки по оси координат X");
             int x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("введите значение точки по оси координат Y");
             int y = Convert.ToInt32(Console.ReadLine());*/
-            Circle circle = new Circle();
-            circle.ToCircumference();
-            circle.ToArea();
-            circle.InACircle();
-            Console.ReadKey();
 
+            double r = 15;
+            double x0 = 3;
+            double y0 = 4;
+            double x1 = 50;
+            double y1 = 4;
+            Circle.L(r);
+            Circle.S(r);
+            Circle.InACircle(x1,y1,r, x0, y0);
+            Console.ReadKey();
         }
 
-    }
-    class Circle
-    {
-        public static double r = 15;
-        public static double x0 = 3;
-        public static double y0 = 4;
 
-        public void ToCircumference()
+
+    }
+    static class Circle
+    {
+
+        public static void L(double r)
         {
             double L = 2 * Math.PI * r;
             Console.WriteLine("Длина окржуности равна {0}", L);
         }
-        public void ToArea()
+        public static void S(double r)
         {
             double S = Math.PI * r * r;
             Console.WriteLine("Площадь круга равна {0}", S);
         }
-        public void InACircle(double x1=50, double y1 = 4)
+        public static void InACircle(double x1, double y1, double r, double x0, double y0)
         {
             if (Math.Pow(x0 - x1, 2) + Math.Pow(y0 - y1, 2) < r * r)
             {
-             Console.WriteLine("точка лежит внутри круга");
-            }
+                Console.WriteLine("точка лежит внутри круга");
+            } 
             else
             {
-             Console.WriteLine("точка лежит вне заданного круга");
+                Console.WriteLine("точка лежит вне заданного круга");
             }
         }
     }
 }
 
-            
 
-           
+
